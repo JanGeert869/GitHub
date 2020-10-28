aed8a1c375adadf9f1813e615398e975088f8fd6
JanGeert869 <67583294+JanGeert869@users.noreply.github.com>
2020-10-28 11:51:32 +0100

    weg
    weg

--- Test-connection/test-connection.ps1
+++
@@ -1,6 +0,0 @@
-foreach($line in Get-Content "C:\Users\JGvdBijl\Google Drive (jangeertalkmaar@gmail.com)\Powershell\Test-connection\A48208SNMP.csv") {
-    if($line -match $regex){
-        # Work here
-        Test-Connection $line -count 1
-    }
-}
